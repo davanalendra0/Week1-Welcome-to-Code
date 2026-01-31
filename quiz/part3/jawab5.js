ca401077e15606a99a44a5d08c6bb180c991740f
Indayani <davanalendra2@gmail.com>
2026-01-31 11:38:17 +0700

    finishing question 5 part3

--- quiz/part3/jawab5.js
+++ quiz/part3/jawab5.js
@@ -1,5 +1,14 @@
 // Problem buatlah bintang seperti berikut
 let input = 5
+let pattern = "";
+for (let i = 1; i <= input; i++) {
+    for (let j = 1; j <= i; j++) {
+        pattern += "*";
+    }
+    pattern += "\n";
+}
+console.log(pattern);
+
 //hasilnya
 //*
 //**
